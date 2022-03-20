999117c9ed02f8d886b06749bae8b2a6b1012677
Sagar Barapatre <b119016@iiit-bh.ac.in>
2022-03-20 14:45:15 +0530

    Add files via upload

---
+++ Arrays/triplets_that_sum_to given_value.cpp
@@ -0,0 +1,25 @@
+bool find3Numbers(int A[], int arr_size, int sum)
+{
+    // Fix the first element as A[i]
+    for (int i = 0; i < arr_size - 2; i++)
+    {
+
+        // Find pair in subarray A[i+1..n-1]
+        // with sum equal to sum - A[i]
+        unordered_set<int> s;
+        int curr_sum = sum - A[i];
+        for (int j = i + 1; j < arr_size; j++)
+        {
+            if (s.find(curr_sum - A[j]) != s.end())
+            {
+                printf("Triplet is %d, %d, %d", A[i],
+                       A[j], curr_sum - A[j]);
+                return true;
+            }
+            s.insert(A[j]);
+        }
+    }
+
+    // If we reach here, then no triplet was found
+    return false;
+}
